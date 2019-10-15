c5c6d7808378d162616ef35214489f4203fb11f2
Lucas Ferreira <ferreira.lucas639@gmail.com>
2019-10-15 14:10:17 -0300

    first commit

---
+++ orcarmento.py
@@ -0,0 +1,42 @@
+from getDataRevit import *
+from sinapiInsumosDesonerado import *
+import pandas as pd
+import os
+
+codigos = getAllCodigos()
+quantidades = getAllQuantidades()
+precosUnitarios = []
+precosTotais = []
+unidades = []
+descricoes = []
+writer = pd.ExcelWriter('orcamento.xlsx', engine='xlsxwriter')
+
+i = 0
+total = 0 
+
+while(i < len(codigos)):
+    precosUnitarios.append(precoUnitario(codigos[i]))
+    precosTotais.append(float("{0:.2f}".format(precosUnitarios[i]*quantidades[i])))
+    unidades.append(unidadeMedida(codigos[i]))
+    descricoes.append(descricao(codigos[i]))
+    total += precosTotais[i]
+    i += 1
+
+codigos.append(" ")
+descricoes.append(" ")
+unidades.append(" ")
+quantidades.append(" ")
+precosUnitarios.append("Valor total:")
+precosTotais.append(total)
+
+data = {'Codigo':codigos,'Descricao':descricoes,'Unidade':unidades,'Quantidade':quantidades,'Preco Unitario':precosUnitarios,'Preco total':precosTotais}
+
+df = pd.DataFrame(data)
+
+try:
+    df.to_excel(writer,sheet_name='orcamento1')
+    writer.save()
+    os.system("clear")
+    print("Planilha criada com sucesso!!!\n")
+except:
+    print("Falha ao exportar a planilha\n")
---
+++ sinapiInsumosDesonerado.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+def openXls():
+    xls = pd.ExcelFile("SINAPI_Preco_Ref_Insumos_MA_201908_Desonerado.xls")
+    sheet = xls.parse(0)
+    return sheet
+
+def precoUnitario(codigo):
+    try:
+        valor = consultaPorCodigo(codigo)[4]
+        rst = ""
+        for n in valor:
+            if n != "." and n != ",":
+                rst += n
+            else:
+                if n == ".":
+                    rst += ""
+                if n == ",":
+                    rst += "."
+        return float(rst)
+    except:
+        print("O codigo " + str(codigo) + " nao existe")
+        exit(0)
+        return "erro"
+
+def descricao(codigo):
+    valor = consultaPorCodigo(codigo)[1]
+    return valor 
+
+def unidadeMedida(codigo):
+    return consultaPorCodigo(codigo)[2]
+
+def consultaPorCodigo(codigo):
+    result = []
+    i = 0
+    sheet = openXls()
+    while i<len(sheet.values):
+        if str(sheet.values[i][0]) == str(codigo):
+            result = sheet.values[i]
+        i += 1
+    return result
+
+def imprimeResultado(codigo):
+    r = consultaPorCodigo(codigo)
+    if len(r) == 5:
+        print("Codigo: " + str(r[0]))
+        print("Descricao: " + str(r[1]))
+        print("Unidade de Medida: " + str(r[2]))
+        print("Origem do preco: " + str(r[3]))
+        print("Preco mediano: R$ " + str(r[4]))
+    else:
+        print("Codigo incorreto")
+
