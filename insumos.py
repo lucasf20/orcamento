75043253961285bb6fcbf75eb5c04b77b12aa03b
Lucas Ferreira <ferreira.lucas639@gmail.com>
2019-11-05 19:58:01 -0300

    otimizando geração de planilhas

---
+++ insumos.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import re
+
+class Insumos:
+    codigo = ''
+    descricao = ''
+    precoUnitario = 0
+    unidadeMedida = ''
+    
+    def openXls(self):
+        xls = pd.ExcelFile(r"C:\Users\lucas\Desktop\dynamo_sinapi\orcamento-master\SINAPI_Preco_Ref_Insumos_MA_201908_Desonerado.xls")
+        sheet = xls.parse(0)
+        return sheet
+
+    '''def precoUnitario(self,codigo):
+        try:
+            valor = self.consultaPorCodigo(codigo)[4]
+            rst = ""
+            for n in valor:
+                if n != "." and n != ",":
+                    rst += n
+                else:
+                    if n == ".":
+                        rst += ""
+                    if n == ",":
+                        rst += "."
+            return float(rst)
+        except:
+            print("O codigo " + str(codigo) + " nao existe")
+            exit(0)
+            return "erro"'''
+
+    '''def descricao(self,codigo):
+        valor = self.consultaPorCodigo(codigo)[1]
+        return valor '''
+
+    '''def unidadeMedida(self,codigo):
+        return self.consultaPorCodigo(codigo)[2]'''
+
+    def consultaPorCodigo(self,codigo):
+        result = []
+        i = 0
+        sheet = self.openXls()
+        while i<len(sheet.values):
+            if str(sheet.values[i][0]) == str(codigo):
+                result = sheet.values[i]
+                break
+            i += 1
+        self.codigo = result[0]
+        self.descricao = result[1]
+        self.unidadeMedida = result[2]
+        self.precoUnitario = self.convertFloat(result[4])
+        #return result
+
+    '''def imprimeResultado(self,codigo):
+        r = self.consultaPorCodigo(codigo)
+        if len(r) == 5:
+            print("Codigo: " + str(r[0]))
+            print("Descricao: " + str(r[1]))
+            print("Unidade de Medida: " + str(r[2]))
+            print("Origem do preco: " + str(r[3]))
+            print("Preco mediano: R$ " + str(r[4]))
+        else:
+            print("Codigo incorreto")'''
+
+    def consultaPorDescricao(self,descri):
+        pattern = ".*(" + str(descri).upper() + ").*"
+        planilha = self.openXls().values
+        des_material = [str(linha[1]) for linha in planilha]
+        cod = [linha[0] for linha in planilha]
+        matches = []
+        i = 0
+        n = 0
+        while i < len(des_material):
+            if re.match(pattern,des_material[i]):
+                matches.append(planilha[i])
+                n += 1
+            i += 1
+        return [matches,n]
+
+    def convertFloat(self,numero):
+        rst = ''
+        for n in numero:
+            if n != '.' and n !=',':
+                rst += n
+            else:
+                if n == ',':
+                    rst += '.'
+        return rst
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from composicao import *
-from sinapiInsumosDesonerado import *
+from insumos import *
 from getDataRevit import *
 import pandas as pd 
 import numpy as np
+import datetime
+
+print("Processo iniciado em " + str(datetime.datetime.now()))
 
 ins = Insumos()
 
@@ -15,6 +18,16 @@ codcomp = rvt.codigos
 insumosPorComposicao = [comp.achaInsumosPorComposicao(cod) for cod in codcomp]
 
 quantidade = rvt.qntidades
+
+def desconverter(numero):
+    numero = str(numero)
+    rst =""
+    for n in numero:
+        if n == ".":
+            rst += ","
+        else:
+            rst += n
+    return rst
 
 def insumos(composicao):
     codigo = 0
@@ -32,10 +45,11 @@ def insumos(composicao):
             if type(item) == list:
                 linha += (insumos(item))
             else:
+                ins.consultaPorCodigo(item)
                 codigo = item
-                descricao = ins.descricao(codigo)
-                unidade = ins.unidadeMedida(codigo)
-                precoUnitario = ins.precoUnitario(codigo)
+                descricao = ins.descricao
+                unidade = ins.unidadeMedida
+                precoUnitario = desconverter(ins.precoUnitario)
                 linha.append(['',codigo,descricao,unidade,precoUnitario])
                 i += 1
         indice += 1
@@ -54,7 +68,7 @@ def insum():
         dados.append(['     ','-','     ','     ','     ','     '])
     return dados
 
-writer = pd.ExcelWriter('teste.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('new.xlsx', engine='xlsxwriter')
 
 ct1 = custototal()
 
@@ -65,17 +79,20 @@ qnts = [c[1] for c in ct1]
 da = insum()
 
 i = 0
+total = 0
 
 for d in da:
     if d[1] != '-':
-        d.append("{0:.2f}".format(qnts[i])) 
-        d.append("{0:.2f}".format(ct[i])) 
+        d.append(desconverter(qnts[i])) 
+        d.append(desconverter("{0:.2f}".format(ct[i])))
+        total += ct[i]
         i+= 1
     else:
         if len(d) < 6:
             d.append(" ")
         d.append(" ")
 
+da.append(['','','','','','CUSTO TOTAL:',desconverter("{0:.2f}".format(total))])
 
 data = pd.DataFrame(np.array(da),columns = ['CODIGO COMPOSICAO','CODIGO INSUMO' ,'DESCRICAO','UNIDADE','PRECO UNITARIO','QUANTIDADE','CUSTO TOTAL'])
 
@@ -84,5 +101,3 @@ data.to_excel(writer, sheet_name = 'orcamento 1')
 writer.save()
 
 print("Planilha exportada com sucesso")
-
-
